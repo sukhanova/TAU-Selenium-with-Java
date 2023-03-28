398af4a1a2cf3c451840ed1041cd24aec53854f7
sukhanova <verusia.s@gmail.com>
2023-03-28 18:26:10 -0500

    Added Frames exercise solution

--- webdriver_java/src/main/java/pages/HomePage.java
+++ webdriver_java/src/main/java/pages/HomePage.java
@@ -63,6 +63,11 @@ public class HomePage {
         return new WysiwygEditorPage(driver);
     }
 
+    public FramesPage clickFramesPage(){
+        clickLink("Frames");
+        return new FramesPage(driver);
+    }
+
     private void clickLink(String linkText){
         driver.findElement(By.linkText(linkText)).click();
     }
